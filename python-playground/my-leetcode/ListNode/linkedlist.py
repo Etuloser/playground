989762c310e28b61de8e3e5cc3c48f4a7b869e80
Eles <1494136313@qq.com>
2022-08-02 21:15:08 +0800

    chore: add linkedlist example

--- python-playground/my-leetcode/ListNode/linkedlist.py
+++ python-playground/my-leetcode/ListNode/linkedlist.py
@@ -3,16 +3,69 @@ https://github.com/keon/algorithms
 """
 
 
-class SinglyLinkedListNode(object):
-    def __init__(self, value):
-        self.value = value
+class SingleNode(object):
+    def __init__(self, val) -> None:
+        self.val = val
         self.next = None
 
 
+class SingleLinkList(object):
+    def __init__(self) -> None:
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def travel(self):
+        cur = self.head
+        while cur:
+            print(cur.val)
+            cur = cur.next
+
+    def append(self, val):
+        node = SingleNode(val)
+        if self.is_empty():
+            self.head = node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = node
+
+
+ltm_info1 = {
+    'hostname': 'SZASHO-FB4-SDAW',
+    'vip': '12.255.25.39',
+    'mbr': '10.1.255.23,12.68.6.1'
+}
+ltm_info2 = {
+    'hostname': 'SZASHO-FB4-SDAW',
+    'vip': '10.1.255.23',
+    'mbr': '11.31.58.69'
+}
+ltm_info3 = {
+    'hostname': 'SZASHO-FB4-SDAW',
+    'vip': '11.31.58.69',
+    'mbr': '55.1.36.98'
+}
+
+store = [ltm_info1, ltm_info2, ltm_info3]
+
+
+def get_next(node: SingleNode):
+    pre_node_mbr = node.val['mbr'].split(',')
+    for doc in store:
+        if doc['vip'] in pre_node_mbr:
+            return SingleNode(doc)
+    return False
+
+
 if __name__ == '__main__':
-    l1 = SinglyLinkedListNode(10)
-    l2 = SinglyLinkedListNode(12)
-    l1.next = l2
-    while l1:
-        print(l1.value)
-        l1 = l1.next
+    node1 = SingleNode(ltm_info1)
+    l1 = SingleLinkList()
+    l1.head = node1
+    cur = l1.head
+    while cur:
+        cur.next = get_next(cur)
+        cur = cur.next
+    l1.travel()
