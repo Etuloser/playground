bf786ff1f445369212dc92d379db4d97a95b9042
Eles <1494136313@qq.com>
2022-08-10 16:52:00 +0800

    chore: stash

---
+++ python-playground/my-django/drf-example/tutorial/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework import routers
+from tutorial.quickstart import views as quickstart_views
+from django.contrib import admin
+
+router = routers.DefaultRouter()
+router.register(r'users', quickstart_views.UserViewSet)
+router.register(r'groups', quickstart_views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
+]
