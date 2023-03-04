bafcfcdaa7b9a560dd763a1ce678d1205dc120be
Eles <1494136313@qq.com>
2023-03-04 13:44:39 +0800

    chore: new branch go

--- python/my-kafka/consumer.py
+++
@@ -1,8 +0,0 @@
-from kafka import KafkaConsumer
-consumer = KafkaConsumer(
-    'test',
-    bootstrap_servers='119.91.25.133:30092',
-    auto_offset_reset='latest'
-)
-for msg in consumer:
-    print(msg)
