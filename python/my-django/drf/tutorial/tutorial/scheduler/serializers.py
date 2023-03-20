e621bf4ba39ed7968ac1826783a3660115d088ea
Eles <1494136313@qq.com>
2023-03-20 19:57:28 +0800

    feature: add nextjs branch

--- python/my-django/drf/tutorial/tutorial/scheduler/serializers.py
+++
@@ -1,8 +0,0 @@
-from django_apscheduler.models import DjangoJob
-from rest_framework.serializers import ModelSerializer
-
-
-class DjangoJobSerializer(ModelSerializer):
-    class Meta:
-        model = DjangoJob
-        fields = ['id', 'next_run_time', 'job_state', 'url']
