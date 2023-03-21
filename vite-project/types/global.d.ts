e11a6ced57ccc001115b946257b6f6a39f19c6fb
yi.zeng <yi.zeng@ez-cloud.com.cn>
2023-03-21 18:50:32 +0800

    chore: stash

---
+++ vite-project/types/global.d.ts
@@ -0,0 +1,22 @@
+interface ServerConfigs {
+    Version?: string;
+    Title?: string;
+    FixedHeader?: boolean;
+    HiddenSideBar?: boolean;
+    MultiTagsCache?: boolean;
+    KeepAlive?: boolean;
+    Locale?: string;
+    Layout?: string;
+    Theme?: string;
+    DarkMode?: boolean;
+    Grey?: boolean;
+    Weak?: boolean;
+    HideTabs?: boolean;
+    SidebarStatus?: boolean;
+    EpThemeColor?: string;
+    ShowLogo?: boolean;
+    ShowModel?: string;
+    MenuArrowIconNoTransition?: boolean;
+    CachingAsyncRoutes?: boolean;
+    TooltipEffect?: Effect;
+}
