9ac6f09bb4a46f50493512dfbbbd7819080719a6
Eles <1494136313@qq.com>
2023-03-28 18:30:55 +0800

    chore: stash

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,1 +1,11 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  env: {
+    customKey: 'my-value',
+  },
+}
+
+module.exports = nextConfig
--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,10 +1,9 @@
 import "../styles/globals.css";
 import Layout from "../components/layout";
-
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps}/>
+      <Component {...pageProps} />
     </Layout>
   );
 }
