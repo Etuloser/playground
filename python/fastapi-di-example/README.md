2a8c72af464a420cc3f66f8a300bddd8d42c996e
Eles <1494136313@qq.com>
2023-03-31 22:00:28 +0800

    feature: add di example

---
+++ python/fastapi-di-example/README.md
@@ -0,0 +1,5 @@
+# FastAPI + Dependency Injector Example
+
+> Library reference
+>
+> [python-dependency-injector/examples/miniapps/fastapi at master · ets-labs/python-dependency-injector (github.com)](https://github.com/ets-labs/python-dependency-injector/tree/master/examples/miniapps/fastapi)
---
+++ python/fastapi-di-example/giphynavigator/giphy.py
@@ -0,0 +1,23 @@
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
+
+
+class GiphyClient:
+    API_URL = "https://api.giphy.com/v1"
+
+    def __init__(self, api_key, timeout) -> None:
+        self._api_key = api_key
+        self._timeout = ClientTimeout(timeout)
+
+    async def search(self, query, limit):
+        """Make search API call and return result."""
+        url = f"{self.API_URL}/gifs/search"
+        params = {
+            "q": query,
+            "api_key": self._api_key,
+            "limit": limit,
+        }
+        async with ClientSession(timeout=self._timeout, connector=TCPConnector(verify_ssl=False)) as session:
+            async with session.get(url, params=params) as response:
+                if response.status != 200:
+                    response.raise_for_status()
+                return await response.json()
