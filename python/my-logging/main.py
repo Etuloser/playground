03850f7d3b9ad200dcc15cb7273b9dfda3d081a4
Eles <1494136313@qq.com>
2023-04-03 23:28:34 +0800

    chore: stash

--- python/my-logging/main.py
+++ python/my-logging/main.py
@@ -1,3 +1,28 @@
+# coding=utf-8
 import logging
-logging.warning('Watch out!')
+import time
+
+logging.warning("Watch out!")  # WARNING:root:Watch out!
+
+# default logging level is waring
+# so info level log will not print
+# log level: DEBUG->INFO->WARNING->ERROR->CRITICAL
 logging.info('I told you so')
+
+logging.basicConfig(filename='example.log', filemode='w',
+                    encoding='utf-8', level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+
+def main():
+    logging.basicConfig(filename='example.log', level=logging.INFO)
+    logging.info('Started')
+    time.sleep(1)
+    logging.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
